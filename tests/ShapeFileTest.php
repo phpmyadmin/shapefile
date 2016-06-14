bb485b92906874756d49eb5e2e137143612a1b0b
Michal Čihař <michal@cihar.com>
2016-06-14 13:38:18 +0200

    Add test for saving and loading back test file
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- tests/ShapeFileTest.php
+++ tests/ShapeFileTest.php
@@ -1,5 +1,6 @@
 <?php
 use ShapeFile\ShapeFile;
+use ShapeFile\ShapeRecord;
 
 class ShapeMoFilesTest extends PHPUnit_Framework_TestCase
 {
@@ -31,5 +32,50 @@ class ShapeMoFilesTest extends PHPUnit_Framework_TestCase
             array('data/capitals.*', 652),
             array('data/mexico.*', 32),
         );
+    }
+
+    /**
+     * Tests creating file
+     *
+     * @return void
+     */
+    public function testCreate()
+    {
+        $shp = new ShapeFile(1);
+
+        $record0 = new ShapeRecord(1);
+        $record0->addPoint(array("x" => 482131.764567, "y" => 2143634.39608));
+
+        $record1 = new ShapeRecord(1);
+        $record1->addPoint(array("x" => 472131.764567, "y" => 2143634.39608));
+
+        $record2 = new ShapeRecord(1);
+        $record2->addPoint(array("x" => 492131.764567, "y" => 2143634.39608));
+
+        $shp->addRecord($record0);
+        $shp->addRecord($record1);
+        $shp->addRecord($record2);
+
+        $shp->setDBFHeader(
+            array(
+                array('ID', 'N', 8, 0),
+                array('DESC', 'C', 50, 0)
+            )
+        );
+
+        $shp->records[0]->DBFData['ID'] = '1';
+        $shp->records[0]->DBFData['DESC'] = 'AAAAAAAAA';
+
+        $shp->records[1]->DBFData['ID'] = '2';
+        $shp->records[1]->DBFData['DESC'] = 'BBBBBBBBBB';
+
+        $shp->records[2]->DBFData['ID'] = '3';
+        $shp->records[2]->DBFData['DESC'] = 'CCCCCCCCCCC';
+
+        $shp->saveToFile('./data/test_shape.*');
+
+        $shp = new ShapeFile(1);
+        $shp->loadFromFile('./data/test_shape.*');
+        $this->assertEquals(3, count($shp->records));
     }
 }
