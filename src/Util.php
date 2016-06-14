982cbb5df59e62b92ba5462f3090e85133f5dc68
Michal Čihař <michal@cihar.com>
2016-06-14 16:38:10 +0200

    Remove unused parameter from packDouble
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- src/Util.php
+++ src/Util.php
@@ -39,17 +39,17 @@ class Util {
         return $result;
     }
 
-    public static function packDouble($value, $mode = 'LE') {
+    public static function packDouble($value) {
         $value = (double) $value;
         $bin = pack("d", $value);
 
         //We test if the conversion of an integer (1) is done as LE or BE by default
         switch (pack('L', 1)) {
             case pack('V', 1): //Little Endian
-                $result = ($mode == 'LE') ? $bin : self::swap($bin);
+                $result = $bin;
                 break;
             case pack('N', 1): //Big Endian
-                $result = ($mode == 'BE') ? $bin : self::swap($bin);
+                $result = self::swap($bin);
                 break;
             default: //Some other thing, we just return false
                 $result = FALSE;
