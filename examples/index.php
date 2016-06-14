8d44b83c9e53497cdd31ca5203aadb3af29b4796
Isaac Bennetch <bennetch@gmail.com>
2016-06-14 13:38:51 -0400

    Improve langauge and change program name
    Signed-off-by: Isaac Bennetch <bennetch@gmail.com>

--- examples/index.php
+++ examples/index.php
@@ -2,11 +2,11 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>BytesFall ShapeFiles demo</title>
+<title>ShapeFiles demo</title>
 </head>
 
 <body>
-This demo illustrates some of the posibilities that offers BytesFall ShapeFiles. There are several more, just check the source code to see the methods implemented in the classes ShapeFile and ShapeRecord (the documentation will be released soon).
+This demo illustrates some of the possibile uses of ShapeFiles. There are several more, just check the source code to see the methods implemented in the classes ShapeFile and ShapeRecord.
 <p><a href="create_shapefile.php">Create new ShapeFile</a></p>
 <p><a href="read_point.php">Read Point ShapeFile</a></p>
 <p><a href="read_polygon.php">Read Polygon ShapeFile</a>   </p>
